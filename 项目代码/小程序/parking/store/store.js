22c12cd76695d4394cdd1ce87d885d77b49e3432
zfr010503 <81499865+zfr010503@users.noreply.github.com>
2023-03-26 20:24:29 +0800

    Delete 项目代码/小程序 directory

--- 项目代码/小程序/parking/store/store.js
+++
@@ -1,9 +0,0 @@
-import {action, observable} from 'mobx-miniprogram'
-
-export const store = observable({
-    activeTabBarIndex: 0,
-
-    updateActiveTabBarIndex: action(function(index){
-        this.activeTabBarIndex = index
-    })
-})
